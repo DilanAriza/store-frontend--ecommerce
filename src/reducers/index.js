3484eaa668888c2dfa5e2a8be5ed8fa2db83790e
DilanAriza <dilansantiagoariza@gmail.com>
2021-06-03 23:16:07 -0500

    Verificando que no se dupliquen los productos a la hora de añadirlos al carrito con diferente monto de unidad :), falta revisar una parte importante de la mierda que verifica si el dato tenia o no el mismo monto y lo devuelve por props al componente

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,47 @@ import { actions } from '../actions';
 const reducer = (state, action) => {
     switch (action.type) {
         case actions.addToCart:
-            const exist = state.myCart.find(item => item.id === action.payload.id)
-            if (exist) return {...state }
+            // Verify is exist id in the state.myCart
+            const existId = state.myCart.find(item => item.id === action.payload.id);
 
+            // Variable to ecual or other amont
+            let itemIcual;
+
+            // Verify if this id exist in the state
+            if (existId) {
+
+                state.myCart.map((item) => {
+
+                    // Verify if the id and payload is ecual in the state 
+                    if (item.id === action.payload.id && item.amount === action.payload.amount) {
+                        itemIcual = 'ecual';
+                    }
+
+                    // Verify if the id is ecual and amount isn't ecual, correct this shit! :)
+                    if (item.id === action.payload.id && item.amount !== action.payload.amount) {
+                        item.amount = action.payload.amount;
+                        itemIcual = 'd-amount';
+                    }
+
+                    // return item maped to state (default or updated)
+                    return item;
+                })
+            }
+
+            // return ecual information to props
+            if (itemIcual === 'ecual') {
+                return {...state }
+            }
+
+            // return direfent information to props (IMPORTANT REVIEW THIS SHIT, UPDATE THIS SHIT PLEACE!)
+            if (itemIcual === 'd-amount') {
+                return {
+                    ...state,
+                    myCart: [...state.myCart]
+                }
+            }
+
+            // Return data aditional to props
             return {
                 ...state,
                 myCart: [...state.myCart, action.payload]
