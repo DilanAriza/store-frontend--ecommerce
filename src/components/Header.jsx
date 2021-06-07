1326eb512a3432ba0738d9868256754a75574d0d
DilanAriza <dilansantiagoariza@gmail.com>
2021-06-07 13:59:08 -0500

    Arreglando el navbar con responsive design, falta organizar el slide bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,12 +14,9 @@ import PropTypes from 'prop-types';
 //Classnames
 import classNames from 'classnames'
 
-//Gravatar
-// import gravatar from '../utils/gravatar';
-
-//Statics
-// import logo from '../assets/static/logo-platzi-video-BW2.png';
-// import userIcon from '../assets/static/user-icon.png';
+//Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 //Styles 
 import '../assets/styles/components/Header.scss'
@@ -42,21 +39,43 @@ const Header = props => {
         <header className={headerClass}>
             <div className="container">
                 
-                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <Link className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none text-uppercase fw-bold fs-3 br-10 p-2" to="/">STORE</Link>
-                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start sub-container-header">
+                    <Link className="d-flex align-items-center mb-lg-0 text-dark text-decoration-none text-uppercase fw-bold fs-3 br-10 p-2 store-link-navbar" to="/">STORE</Link>
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-md-0 links-navbar justify-content-around list-nav-items-store">
                         <li><a href="#" className="nav-link px-2 link-dark">Categorías</a></li>
                         <li><a href="#" className="nav-link px-2 link-dark">Productos</a></li>
                         <li><a href="#" className="nav-link px-2 link-dark">Rating</a></li>
+                        <li><button className="nav-link px-2 link-dark" type="button"  data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><FontAwesomeIcon icon={faBars} /></button></li>
                     </ul> 
+                    <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <div>
+                            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+                            </div>
+                            <div className="dropdown mt-3">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                                Dropdown button
+                            </button>
+                            {/* <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul> */}
+                            </div>
+                        </div>
+                    </div>
 
-                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 search-bar">
                         <input type="search" className="form-control" placeholder="Buscar..." />
                     </form>
 
-                    <div className="dropdown text-end">
+                    <div className="dropdown text-end cart-image">
                         <a href="#" className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://image.flaticon.com/icons/png/512/2121/2121815.png" alt="mdo" width="32" height="32" />
+                            <img src="https://image.flaticon.com/icons/png/512/2121/2121815.png" alt="mdo"/>
                         </a>
                         {
                             hasUser ?
@@ -76,16 +95,6 @@ const Header = props => {
                                 </li>
                             </ul>: 
                             <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                                {/* <li>
-                                    <Link className="dropdown-item" to="/my-cart">Mi carrito</Link>    
-                                </li>
-                                <li>
-                                    <Link className="dropdown-item" to="/settings">Configuración</Link>    
-                                </li>
-                                <li>
-                                    <Link className="dropdown-item" to="/acount">Cuenta</Link>    
-                                </li>
-                                <li><hr className="dropdown-divider"/></li> */}
                                 <li>
                                     <Link className="dropdown-item" to="/login">Iniciar Sesión</Link>    
                                 </li>
