6263d2c55d3089617cd7d6de828ca9a2376c4a89
DilanAriza <dilansantiagoariza@gmail.com>
2021-06-07 19:54:45 -0500

    Asignando todas las rutas para render en el servidor con express

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const express = require('express');
-
-let app = express();
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 let server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
