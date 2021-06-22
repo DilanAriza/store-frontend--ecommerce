2aaca4b90e8ff2507f29ac8ebcfb8c64674ede2d
DilanAriza <dilansantiagoariza@gmail.com>
2021-06-22 17:44:52 -0500

    Agregando la mejora del nombre de los componentes al router

--- src/routes/App.routes.jsx
+++ src/routes/App.routes.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '../view/Layouts/Layout.home';
-import CartCotainer from '../view/pages/Cart.page';
-import CheckoutCart from '../view/pages/Checkout-cart.page';
-import Home from '../view/pages/Home.page';
-import Item from '../view/pages/Item.page';
-import NotFound from '../view/pages/NotFound.page';
+import CartPage from '../view/pages/Cart.page';
+import CheckoutCartPage from '../view/pages/Checkout-cart.page';
+import HomePage from '../view/pages/Home.page';
+import ItemPage from '../view/pages/Item.page';
+import NotFoundPage from '../view/pages/NotFound.page';
 
 const App = () => (
     <BrowserRouter>
     <Layout>
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/item/:id" component={Item} />
-            <Route path="/my-cart" component={CartCotainer} />
-            <Route path="/checkout-cart" component={CheckoutCart} />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/item/:id" component={ItemPage} />
+            <Route path="/my-cart" component={CartPage} />
+            <Route path="/checkout-cart" component={CheckoutCartPage} />
             {/* <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/player/:id" component={Player}/> */}
-            <Route component={NotFound}/>
+            <Route component={NotFoundPage}/>
         </Switch>
     </Layout>
     </BrowserRouter>
